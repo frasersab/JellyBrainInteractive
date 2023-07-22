e435590884e5c42196e7ce4dccc6ab68ca3d17a9
Fraser Sabine <frasersab@outlook.com>
2023-07-22 15:29:31 +1000

    got brain import working

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
+import brain5000Json from '../brains/brain5000.json';
+import {JellyBrain, costFuncs, activationFuncs} from '../../node_modules/jellybrain/src/JellyBrain.js'
+
 let brain = new JellyBrain(784, 784, 10, costFuncs.crossEntropy, 0.01, activationFuncs.sigmoid, activationFuncs.softmax);
+brain.importBrain(brain5000Json);
+
