7f17c8173150af2790a3a6207176ebdc678ac073
Fraser Sabine <frasersab@outlook.com>
2024-05-03 11:57:43 -0500

    added probability distribution graph

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,72 @@
 import brain5000Json from '../brains/brain5000.json';
 import brain22000Json from '../brains/brain22000.json';
 import {JellyBrain, costFuncs, activationFuncs} from '../../node_modules/jellybrain/src/JellyBrain.js';
+import Chart from 'chart.js/auto';
 
 // load the brain
 let brain = new JellyBrain(784, 784, 10, costFuncs.crossEntropy, 0.01, activationFuncs.sigmoid, activationFuncs.softmax);
 brain.importBrain(brain22000Json);
 
 
+// setup the canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 28;
 canvas.height = 28;
-let scale = getComputedStyle(document.body).getPropertyValue('--scale');
+let scale = getComputedStyle(document.body).getPropertyValue('--scale');  // getting scale from the stylesheet
 let coord = { x: 0, y: 0 };
 
+
+// setup graph
+let canvasChart = new Chart(
+    document.getElementById('probabilityGraph'),
+    {
+      type: 'bar',
+      data: {
+        labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        datasets: [
+          {
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          }
+        ]
+      },
+      options: {
+        plugins: {
+          legend: {
+              display: false
+          }
+        },
+        responsive: false,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            ticks: {
+              precision: 1,
+              callback: function(value, index, ticks) {
+                        return value + '%';
+                    }
+            },
+            min: 0,
+            max: 100
+          }
+        }
+      }
+    }
+  );
 
 
 // add event listeners
 document.addEventListener("mousedown", start);
 document.addEventListener("mouseup", stop);
 
-// add button click
+
+// add button listeners
 document.getElementById("clearButton").addEventListener("click", clear);
 document.getElementById("guessButton").addEventListener("click", guess);
+document.getElementById("copyButton").addEventListener("click", copyData);
 
+
+// drawing functions
 function reposition(event)
 {
   coord.x = (event.clientX - canvas.offsetLeft) / scale;
@@ -53,23 +96,24 @@ function draw(event)
   ctx.stroke();
 }
 
+function updateChart(probability)
+{
+  canvasChart.data.datasets[0].data = probability.map((value) => value = value * 100)
+  canvasChart.update();
+}
+
+
+// button functions
 function clear()
 {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   guessText.innerHTML = "My guess will show here!";
+  updateChart([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 }
 
 function guess()
 {
-    let image = [];
-    for (let y = 0; y <= 27; y++)
-    {
-        for (let x = 0; x <= 27; x++)
-        {
-            let pixel = ctx.getImageData(x, y, 1, 1);
-            image.push((pixel.data[3]) / (255));
-        }
-    }
+    let image = getImage();
 
     let guessArray = brain.guess(image);
     let guessNumber = guessArray.reduce((bestIndex, currentValue, currentIndex) =>
@@ -78,4 +122,32 @@ function guess()
     }, 0);
     let guessText = document.getElementById("guessText");
     guessText.innerHTML = JSON.stringify(guessNumber);
+    updateChart(guessArray);
+}
+
+function copyData()
+{
+  let image = getImage(false);
+  navigator.clipboard.writeText(JSON.stringify(image));
+  alert("Copied image data.");
+}
+
+// helper functions
+function getImage(squished = true)
+{
+  let image = [];
+  for (let y = 0; y <= 27; y++)
+  {
+      for (let x = 0; x <= 27; x++)
+      {
+          let pixel = ctx.getImageData(x, y, 1, 1).data[3];
+          if (squished)
+            {
+              pixel = pixel / 255
+            }
+          image.push(pixel);
+      }
+  }
+
+  return image;
 }
